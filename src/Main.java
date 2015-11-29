6063d1fd30e9d7087b6ba0f26afa1dcc43fdbedc
Josh Westlake <jwestlake@zerocomplexity.ca>
2015-11-28 20:49:11 -0600

    Initial commit with functioning eclipse project

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import algorithm.PrefixSpan;
+import utils.ReadFile;
+
+/**
+ * Main class
+ *
+ * @author Raúl Moya Reyes <raulmoya.es>
+ * @author Agustín Ruiz Linares <agustruiz.es>
+ */
+public class Main {
+
+    ///Params path
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Read params
+        String paramsPath = "./params.txt";
+        ReadFile rf = new ReadFile((paramsPath));
+        String filePath = rf.readLine();
+        int min_sup = Integer.parseInt(rf.readLine());
+        int max_pat = Integer.parseInt(rf.readLine());
+        
+        //Execute algorithm
+        PrefixSpan a = new PrefixSpan(min_sup, max_pat);
+        a.run(filePath);
+    }
+
+    private void readParams() {
+        //Read params.txt
+    }
+
+}
---
+++ src/algorithm/Transaction.java
@@ -0,0 +1,35 @@
+
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Transaction class
+ * 
+ * @author Raúl Moya Reyes <raulmoya.es>
+ * @author Agustín Ruiz Linares <agustruiz.es>
+ */
+public class Transaction {
+    /// Possition in database
+    public int first;
+    ///List of items in transaction
+    public List<Integer> second;
+
+    /**
+     * Class constructor
+     */
+    public Transaction() {
+        this.first = 0;
+        this.second = new ArrayList();
+    }
+
+    /**
+     * Clear transaction
+     */
+    void clear() {
+        this.first = 0;
+        this.second = new ArrayList();
+    }
+
+}
